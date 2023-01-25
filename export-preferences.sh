5d40d63edaff0cf1985a15795e8190f6ed56ee14
- The Aarnold - <mane.imation9@gmail.com>
2023-01-25 16:13:34 +0100

    Start working...

---
+++ export-preferences.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+zsh --version || echo "Hello"
+
+# omzsh_installer="https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
+
+# Say hello
+
+# Check ZSH
+   # Install ZSH
+# Check $SHELL set to zsh
+   # Set $SHELL
+
+# Check OhMyZsh
+   # Install OhMyZsh
+# Check OMZSH Plugins
+   # Install
+
+# Check powerlevel10k
+   # Install powerlevel10k
+
+# Check vim
+   # install
+# Check vim plugins
+   # Install plugins
+
+# Set preferences/dot-files
+
+# Source preferences
+
